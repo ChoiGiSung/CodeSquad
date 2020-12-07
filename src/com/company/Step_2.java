f0dc576ea1580d69f311486b783e6e38e75e15c2
ChoiGiSung <chlrltjd159@naver.com>
2020-12-07 20:56:37 +0900

    게임시작 메소드작성
    스캐너를 선언하고 무한루프로 사용자입력값을 받는다.
    사용자입력갑중 작은따옴표는 각각소문자로 바꿔준다.
    바꿔준 commend를 commandSwitch함수를 통해
    하나하나에 맞는 함수를 실행시켜준다.
    리턴값이 -1이면 함수를 종료한다.

--- src/com/company/Step_2.java
+++ src/com/company/Step_2.java
@@ -6,4 +6,22 @@ public class Step_2 {
     private String [][]cube={{"R","R","W"},{"G","C","W"},{"G","B","B"}};
 
 
+    public void startGame(){
+        Scanner sc=new Scanner(System.in);
+
+        print(' ');
+        while (true){
+            System.out.print("CUBE>");
+            String commend=sc.nextLine();
+            commend=commend.replace("U'","u").replace("R'","r")
+                    .replace("L'","l").replace("B'","b");
+            //'는 자르기 힘드니까 바꿔서 계산하기
+
+            char[] chars=commend.toCharArray();
+            int result = commandSwitch(chars); //리턴값이 -1이면 끝
+            if (result==-1){
+                break;
+            }
+        }
+    }
 }
