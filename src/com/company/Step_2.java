f0591c2e012bda074a949bd0dd83ab0618e181cc
ChoiGiSung <chlrltjd159@naver.com>
2020-12-07 21:03:13 +0900

    사용자가 R'를 입력할때
    가장 오른쪽 줄을 아래로 한 칸씩 옮겨준다

--- src/com/company/Step_2.java
+++ src/com/company/Step_2.java
@@ -111,4 +111,15 @@ public class Step_2 {
         cube[1][2]=save3;
         cube[2][2]=save1;
     }
+
+    private void RComa(String[][] cube){
+        //가장 오른쪽 줄을 아래로 한 칸 밀기 WWB -> BWW
+        String save1=cube[0][2];
+        String save2=cube[1][2];
+        String save3=cube[2][2];
+
+        cube[0][2]=save3;
+        cube[1][2]=save1;
+        cube[2][2]=save2;
+    }
 }
