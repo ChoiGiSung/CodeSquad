bccb091c07836be45624923a0d908ca5e84efd3b
ChoiGiSung <chlrltjd159@naver.com>
2020-12-08 21:00:11 +0900

    1번 문제 완성
    완성해서 주석처리했음

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,8 +6,8 @@ public class Main {
 
     public static void main(String[] args) {
         //  1번 문제
-        Step_1 step_1=new Step_1();
-        step_1.input();
+//        Step_1 step_1=new Step_1();
+//        step_1.input();
 
         //2번 문제
         Step_2 step_2=new Step_2();
