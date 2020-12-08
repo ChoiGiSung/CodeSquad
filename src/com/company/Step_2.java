17e2083e579c95ab60fbb7a55771a68b5a334e4a
ChoiGiSung <chlrltjd159@naver.com>
2020-12-08 20:24:34 +0900

    Revert "전역변수 사용 줄이기"
    This reverts commit 2b8180d265fe4659a9c1d0ef3175b52135545675.

--- src/com/company/Step_2.java
+++ src/com/company/Step_2.java
@@ -4,14 +4,13 @@ import java.util.Scanner;
 
 public class Step_2 {
     private String [][]cube={{"R","R","W"},{"G","C","W"},{"G","B","B"}};
-    private boolean flag=true;
 
 
     public void startGame(){
         Scanner sc=new Scanner(System.in);
-        String[][]rocalArr=cube.clone(); //로컬 변수로 사용하기
-        print(' ',rocalArr);
-        while (flag){
+
+        print(' ');
+        while (true){
             System.out.print("CUBE>");
             String commend=sc.nextLine();
             commend=commend.replace("U'","u").replace("R'","r")
@@ -19,8 +18,10 @@ public class Step_2 {
             //'는 자르기 힘드니까 바꿔서 계산하기
 
             char[] chars=commend.toCharArray();
-            rocalArr= commandSwitch(chars,rocalArr);
-
+            int result = commandSwitch(chars); //리턴값이 -1이면 끝
+            if (result==-1){
+                break;
+            }
         }
     }
 
