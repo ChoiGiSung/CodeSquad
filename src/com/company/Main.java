96881e6d9ce0d105c30bad85f3f563b0d66b0b8e
ChoiGiSung <chlrltjd159@naver.com>
2020-12-08 19:47:54 +0900

    main문에서 step-1 객체 호출
    객체 호출메서도 변경해서
    이제 input만 호출하면 객체 내부에서 scanner가 돌아간다

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,19 +5,10 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
 
-        System.out.println("단어 하나 입력");
-        String str= sc.next();
-
-        System.out.println("-100~100  값 하나 입력");
-        int i = sc.nextInt();
-
-        System.out.println("우측으로 갈지 좌측으로 갈지 L,R");
-        String hand=sc.next();
-
+    //  1번 문제
         Step_1 step_1=new Step_1();
-        step_1.calc(str,i,hand);
+        step_1.input();
 
     }
 }
