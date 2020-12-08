2d4bcfcb166807a560239d70fc5ebc493c50f9e1
ChoiGiSung <chlrltjd159@naver.com>
2020-12-08 21:01:14 +0900

    스텝투 변경사항 merge 오류해결중
    merge오류 해결중

--- src/com/company/Step_2.java
+++ src/com/company/Step_2.java
@@ -4,13 +4,16 @@ import java.util.Scanner;
 
 public class Step_2 {
     private String [][]cube={{"R","R","W"},{"G","C","W"},{"G","B","B"}};
+    private boolean flag=true;
 
 
     public void startGame(){
         Scanner sc=new Scanner(System.in);
+        String[][]rocalArr=cube.clone(); //로컬 변수로 사용하기
 
-        print(' ');
-        while (true){
+        print(' ',rocalArr);
+
+        while (flag){
             System.out.print("CUBE>");
             String commend=sc.nextLine();
             commend=commend.replace("U'","u").replace("R'","r")
@@ -18,21 +21,19 @@ public class Step_2 {
             //'는 자르기 힘드니까 바꿔서 계산하기
 
             char[] chars=commend.toCharArray();
-            int result = commandSwitch(chars); //리턴값이 -1이면 끝
-            if (result==-1){
-                break;
-            }
+            rocalArr= commandSwitch(chars,rocalArr);
+
         }
     }
 
-    private void print(char c){
+    private void print(char c,String[][]rocalArr){
         String str=c+"";
         str=str.replace("u","U'").replace("r","R'")
                 .replace("l","L'").replace("b","B'");
         System.out.println(str);
-        for (int i = 0; i < cube.length; i++) {
-            for (int j=0;j<cube[0].length;j++){
-                System.out.print(cube[i][j]+" ");
+        for (int i = 0; i < rocalArr.length; i++) {
+            for (int j=0;j<rocalArr[0].length;j++){
+                System.out.print(rocalArr[i][j]+" ");
             }
             System.out.println("");
         }
