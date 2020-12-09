3caf714c2fdadd5a4c7280df4323de30ea81dcce
ChoiGiSung <chlrltjd159@naver.com>
2020-12-09 21:50:34 +0900

    R이 입력됬을떄의 기능
    R이 입력되면 U,F,D,B의 면이 움직인다,
    R의 면은 시계방향으로 회전한다.

--- src/com/company/Step_3.java
+++ src/com/company/Step_3.java
@@ -59,6 +59,31 @@ public class Step_3 {
         }
 
     }
+    public void R(){
+        //입력이 R일때
+        String []temp={UCube[0][2],UCube[1][2],UCube[2][2]};
+
+        //옆면들 옮기기 시작
+        UCube[0][2]=FCube[0][2];
+        UCube[1][2]=FCube[1][2];
+        UCube[2][2]=FCube[2][2];
+
+        FCube[0][2]=DCube[0][2];
+        FCube[1][2]=DCube[1][2];
+        FCube[2][2]=DCube[2][2];
+
+        DCube[0][2]=BCube[2][0];
+        DCube[1][2]=BCube[1][0];
+        DCube[2][2]=BCube[0][0];
+
+        BCube[2][0]=temp[0];
+        BCube[1][0]=temp[1];
+        BCube[0][0]=temp[2];
+
+        //이제 평면 부분 돌기
+        RCube=turnClock(RCube);
+    }
+
     public void B(){
         //입력이 B일때
         String []temp={UCube[0][0],UCube[0][1],UCube[0][2]};
