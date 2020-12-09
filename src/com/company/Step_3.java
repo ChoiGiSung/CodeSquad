43eef36850b7e867fae588f9dd437a70ba42689f
ChoiGiSung <chlrltjd159@naver.com>
2020-12-09 21:46:40 +0900

    F가 입력됬을때의 동작
    F가 입력되면 U,R,L,D의 큐브면이 돌아가야 한다.
    배열의 인덱스를 조작해서 옮겼다.

--- src/com/company/Step_3.java
+++ src/com/company/Step_3.java
@@ -59,7 +59,31 @@ public class Step_3 {
         }
 
     }
+    public void F(){
+        //입력이 F일때
+        String []temp={UCube[2][0],UCube[2][1],UCube[2][2]};
 
+        //옆면들 옮기기 시작
+        UCube[2][0]=LCube[2][2];
+        UCube[2][1]=LCube[1][2];
+        UCube[2][2]=LCube[0][2];
+
+        LCube[0][2]=DCube[0][0];
+        LCube[1][2]=DCube[0][1];
+        LCube[2][2]=DCube[0][2];
+
+        DCube[0][0]=RCube[2][0];
+        DCube[0][1]=RCube[1][0];
+        DCube[0][2]=RCube[0][0];
+
+        RCube[0][0]=temp[0];
+        RCube[1][0]=temp[1];
+        RCube[2][0]=temp[2];
+
+        //이제 평면 부분 돌기
+        FCube=turnClock(FCube);
+
+    }
     public String[][] turnClock(String[][]cube){
         //시계 방향으로 턴
         String[][]rocalCube=new String[3][3];
