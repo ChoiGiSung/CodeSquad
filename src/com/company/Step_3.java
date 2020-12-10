209af50afe0ea57d6142190f1864cce3bde30960
ChoiGiSung <chlrltjd159@naver.com>
2020-12-10 20:55:08 +0900

    print시 사용하는 문자를 원상복귀시키자
    print시 문자를 하나 받아서 출력하는데
    그 문자는 이전 계산에서 작은따옴표가 붙으면 소문자로
    바꿨기 때문에 다시 원상복귀 시켜준다

--- src/com/company/Step_3.java
+++ src/com/company/Step_3.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Step_3 {
     String [][]FCube={{"1","2","3"},
             {"4","5","6"},
@@ -21,16 +23,29 @@ public class Step_3 {
             {"7","8","9"}};//하
 
 
+
+
+
+    private String replaceCommendChar(char c){
+        //원복
+        String str=c+""; 
+        String result=str.replace("u","U'").replace("r","R'")
+                .replace("l","L'").replace("b","B'")
+                .replace("f","F'").replace("d","D'");
+        return result;
+    }
     private void printsolo(String[][]cube){
         for (int i = 0; i < cube.length; i++) {
+            System.out.print("\t"+"\t");
             for (int j = 0; j < cube[0].length; j++) {
                 System.out.print(cube[i][j]);
             }
             System.out.println(" ");
         }
     }
-    public void print(){
-        System.out.println("");
+    public void print(char c){
+        String str = replaceCommendChar(c); //글자 하나씩 출력
+        System.out.println("\n"+str);
         printsolo(UCube);
         for (int i = 0; i < UCube.length; i++) {
             StringBuilder str1=new StringBuilder();
@@ -165,6 +180,9 @@ public class Step_3 {
         FCube=turnClock(FCube);//이제 평면 부분 돌기
 
     }
+
+
+
     public String[][] turnClock(String[][]cube){
         //시계 방향으로 턴
         String[][]rocalCube=new String[3][3];
