ac8230df061f867fdd7a06bac022e2965df839d1
ChoiGiSung <chlrltjd159@naver.com>
2020-12-10 20:58:06 +0900

    작은따옴표가 붙어 있을 경우 반시계로 회전
    보이는 면이 반시계 방향으로 인덱스가
    움직여야한다

--- src/com/company/Step_3.java
+++ src/com/company/Step_3.java
@@ -197,7 +197,24 @@ public class Step_3 {
 
     }
 
+    private String[][] turnAnticlockwise(String[][] cube) {
+        //반시계 방향으로 턴
+        String[][]rocalCube=new String[3][3];
 
+        rocalCube[2][0]=cube[0][0];
+        rocalCube[1][0]=cube[0][1];
+        rocalCube[0][0]=cube[0][2];
+
+        rocalCube[2][1]=cube[1][0];
+        rocalCube[1][1]=cube[1][1];
+        rocalCube[0][1]=cube[1][2];
+
+        rocalCube[2][2]=cube[2][0];
+        rocalCube[1][2]=cube[2][1];
+        rocalCube[0][2]=cube[2][2];
+
+        return rocalCube;
+    }
 
     public String[][] turnClock(String[][]cube){
         //시계 방향으로 턴
